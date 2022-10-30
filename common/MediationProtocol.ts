098ef9a445181fb185710757f0cbdb5dc9e116bf
Jason Benz <j.benz@webbenz.ch>
2022-10-30 13:21:25 +0100

    Added MediationProtocol

---
+++ common/MediationProtocol.ts
@@ -0,0 +1,45 @@
+export type MediationEventCallback = (...args: any[]) => void;
+
+export class MediationProtocol {
+    private stream: any;
+    private listeners: Map<string, MediationEventCallback[]>;
+
+    constructor(stream: any) {
+        this.stream = stream;
+        this.listeners = new Map<string, MediationEventCallback[]>();
+
+        stream.on('get_peers', (...args: any[]) => this.listeners.get('get_peers')?.forEach((callback) => callback(...args)));
+        stream.on('peers', (...args: any[]) => this.listeners.get('peers')?.forEach((callback) => callback(...args)));
+        stream.on('signal', (...args: any[]) => this.listeners.get('signal')?.forEach((callback) => callback(...args)));
+        stream.on('announce', (...args: any[]) => this.listeners.get('announce')?.forEach((callback) => callback(...args)));
+        stream.on('finish', (...args: any[]) => this.listeners.get('finish')?.forEach((callback) => callback(...args)));
+    }
+
+    on(event: string, callback: MediationEventCallback) {
+        if (!this.listeners.get(event)) {
+            this.listeners.set(event, []);
+        }
+
+        this.listeners.get(event)?.push(callback);
+    }
+
+    public get_peers(fullHash: string) {
+        this.stream.emit('get_peers', fullHash);
+    }
+
+    public peers(fullHash: string, peerList: string[]) {
+        this.stream.emit('peers', fullHash, peerList);
+    }
+
+    public signal(senderPeerId: string, receiverPeerId: string, signalData: string) {
+        this.stream.emit('signal', senderPeerId, receiverPeerId, signalData);
+    }
+
+    public announce(seederPeerId: string, fullHash: string) {
+        this.stream.emit('announce', seederPeerId, fullHash);
+    }
+
+    public finish(seederPeerId: string, fullHash: string) {
+        this.stream.emit('finish', seederPeerId, fullHash);
+    }
+}
