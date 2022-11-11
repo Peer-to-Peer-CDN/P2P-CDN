83cec194a9593516acc78547300fd64dbfa2443e
Jason Benz <j.benz@webbenz.ch>
2022-11-11 13:07:29 +0100

    Updated mediation protocol

--- common/MediationProtocol.ts
+++ common/MediationProtocol.ts
@@ -48,8 +48,8 @@ export class MediationProtocol {
         this.stream.emit('peers', fullHash, peerList);
     }
 
-    public signal(full_hash:string, receiverPeerId: string, signalData: string) {
-        this.stream.emit('signal',full_hash, receiverPeerId, signalData);
+    public signal(receiverPeerId: string, signalData: string) {
+        this.stream.emit('signal', receiverPeerId, signalData);
     }
 
     public announce(fullHash: string) {
