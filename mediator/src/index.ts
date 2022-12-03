adb65b34628f9e143a2f60ada7cfc5e71c048fcd
Jason Benz <j.benz@webbenz.ch>
2022-12-03 09:48:00 +0100

    Use ConnectionKeyWords in index.ts

--- mediator/src/index.ts
+++ mediator/src/index.ts
@@ -2,9 +2,17 @@ import { Server } from "socket.io";
 import { MediationServer } from "./MediationServer";
 
 // TEST
+/* Expected Output:
+ * dht-node now listening on  5556
+ * dht-node now listening on  5555
+ * it works aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa. [ '111111111111111111111111111111111111111.' ]
+ * recieved signal for aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa. from 222222222222222222222222222222222222222. with message hello world please connect
+ * ...recieved signal for aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa. from 111111111111111111111111111111111111111. with message i would love to connect bro
+ */
+
 /*
 import {io} from "socket.io-client";
-import {ConnectionType, MediationProtocol} from "../../common/MediationProtocol";
+import {ConnectionKeyWords, ConnectionType, MediationProtocol} from "../../common/MediationProtocol";
 
 let socket1: any;
 let socket2: any;
@@ -13,10 +21,10 @@ setTimeout(() => {
     socket1 = io("ws://localhost:8888");
     const peer1 = new MediationProtocol(socket1);
     peer1.handshake("111111111111111111111111111111111111111.", ConnectionType.MEDIATION);
-    peer1.on('established', () => {
+    peer1.on(ConnectionKeyWords.ESTABLISHED, () => {
         peer1.announce("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.");
-        peer1.on('signal', (fullhash, senderPeerId, data) => {
-            console.log("recieved signal for", fullhash, "from", senderPeerId, "with message", data);
+        peer1.on(ConnectionKeyWords.SIGNAL, (full_hash, senderPeerId, data) => {
+            console.log("received signal for", full_hash, "from", senderPeerId, "with message", data);
 
             peer1.signal("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.", senderPeerId, "i would love to connect bro");
         });
@@ -28,19 +36,19 @@ setTimeout(() => {
     const peer2 = new MediationProtocol(socket2);
 
     peer2.handshake("222222222222222222222222222222222222222.", ConnectionType.MEDIATION);
-    peer2.on('established', () => {
+    peer2.on(ConnectionKeyWords.ESTABLISHED, () => {
         peer2.get_peers("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.");
     });
 
-    peer2.on('peers', (full_hash: string, peerList: string[]) => {
+    peer2.on(ConnectionKeyWords.PEERS, (full_hash: string, peerList: string[]) => {
         console.log("it works", full_hash, peerList);
         peer2.signal("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.", peerList[0], "hello world please connect");
     });
-    peer2.on('signal', (fullhash, senderPeerid, data) => {
-        console.log("...recieved signal for", fullhash, "from", senderPeerid, "with message", data);
+    peer2.on(ConnectionKeyWords.SIGNAL, (full_hash, senderPeerid, data) => {
+        console.log("...received signal for", full_hash, "from", senderPeerid, "with message", data);
     });
-}, 2000);*/
-
+}, 2000);
+*/
 
 setTimeout(() => {
     let mediator1 = new MediationServer(new Server(8888, {cors: {origin: '*'}}), ["127.0.0.1:5556"], 5555, 8888);
