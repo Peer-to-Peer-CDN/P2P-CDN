f681955ee5f761f088256545dacc52c0727e8c15
Jason Benz <j.benz@webbenz.ch>
2022-12-09 16:34:39 +0100

    Deleted old mediator

--- old_mediator/common/MediatorAddress.ts
+++
@@ -1,7 +0,0 @@
-export class MediatorAddress {
-    public url: string;
-
-    constructor(mediatorAddress: string, mediatorPort: number) {
-        this.url = mediatorAddress + ":" + mediatorPort;
-    }
-}
